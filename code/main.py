8f253d86054bb9397d4c69e864ff4534292d1017
davidbyd <davidbyd@gmail.com>
2021-05-18 22:40:12 +0300

    testing

--- code/main.py
+++ code/main.py
@@ -29,6 +29,42 @@ def sperate_tags(file_path_read,file_path_write):
                 words = [re.split('_', word)[0] for word in splited_words] 
                 f_s.write(" ".join(words)+"\n")
 
+def test():
+    file_1=join(data_path,"tags_1000_v2test1.words")
+    file_2=join(data_path, 'test1.wtag')
+    dict_res_by_tag = {}
+    with open(file_1) as f_1, open(file_2) as f_2:
+        lines_1=f_1.readlines()
+        lines_2=f_2.readlines()
+        true_count = 0
+        false_count = 0
+        for i in range(len(lines_1)):
+            
+            splited_words2 = re.split('[ \n]', lines_2[i])
+            splited_words1 = re.split('[ \n]', lines_1[i])
+            del splited_words1[-1]
+            del splited_words2[-1]
+            for w_idx in range(len(splited_words1)-1):
+               
+
+                cur_word_1, cur_tag_1 = splited_words1[w_idx].split('_')
+                cur_word_2, cur_tag_2 = splited_words2[w_idx].split('_')
+                if cur_tag_2 not in dict_res_by_tag.keys():
+                    dict_res_by_tag[cur_tag_2] = {}
+                    dict_res_by_tag[cur_tag_2][True] = 0
+                    dict_res_by_tag[cur_tag_2][False] = 0
+                if cur_tag_1 != cur_tag_2:
+                     dict_res_by_tag[cur_tag_2][False] +=1
+                     false_count +=1
+                else:
+                    dict_res_by_tag[cur_tag_2][True] +=1   
+                    true_count +=1
+
+    for pos_tag, tag_dict in dict_res_by_tag.items():
+        if tag_dict[True]/(tag_dict[True] + tag_dict[False]) * 100 < 95:
+            print(f'Results for tag {pos_tag} are correct at : {tag_dict[True]/(tag_dict[True] + tag_dict[False]) * 100} of total: {tag_dict[True] + tag_dict[False]}')
+    
+    print(f'Results for general are correct at : {true_count/(true_count + false_count) * 100}')
 
 if __name__ == '__main__':
     
@@ -36,18 +72,18 @@ if __name__ == '__main__':
     curr_dir = os.path.dirname(os.path.abspath(__file__))
     data_path = join(curr_dir, "..", 'data')
     train_file_1 = join(data_path, 'train1.wtag')
-    L=LLM(1000,4999,data_path)
+    L=LLM(10,2000,data_path)
     L.train(train_file_1)
     #Viterbi_test(L)
-    #cProfile.runctx("Viterbi_test(L)",{"Viterbi_test":Viterbi_test},{"L": L},"qe")
-    test_file_1= join(data_path, 'test1.wtag')
-    comp_file_1= join(data_path, 'comp1.words')
-    test_file_without_tags_1= join(data_path, 'v2test1.words')
-    if not  os.path.isfile(test_file_without_tags_1):
-        sperate_tags(test_file_1,test_file_without_tags_1)
-    L.tag_file('v2test1.words')
-    L.tag_file('comp1.words')
-
+    #cProfile.runctx("Viterbi_test(L)",{"Viterbi_test":Viterbi_test},{"L": L})
+    # test_file_1= join(data_path, 'test1.wtag')
+    # comp_file_1= join(data_path, 'comp1.words')
+    # test_file_without_tags_1= join(data_path, 'v2test1.words')
+    # if not  os.path.isfile(test_file_without_tags_1):
+    #     sperate_tags(test_file_1,test_file_without_tags_1)
+    # L.tag_file('v2test1.words')
+    # L.tag_file('comp1.words')
+    
 
 
 
